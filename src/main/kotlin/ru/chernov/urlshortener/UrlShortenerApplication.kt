eda2ebfd2024be868b44f9c9f3b9aac5c4604f39
Pavel Chernov <p.chernov@itigris.ru>
2022-11-06 12:37:38 +0600

    init

---
+++ src/main/kotlin/ru/chernov/urlshortener/UrlShortenerApplication.kt
@@ -0,0 +1,13 @@
+package ru.chernov.urlshortener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlShortenerApplication {
+
+    fun main(args: Array<String>) {
+        runApplication<UrlShortenerApplication>(*args)
+    }
+
+}
