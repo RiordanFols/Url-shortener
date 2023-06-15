4e5a971fdef8c8dcfd1cd569510a8a5e1f421219
Pavel Chernov <p.chernov@itigris.ru>
2023-06-15 21:59:36 +0300

    finish tests

--- src/main/java/ru/chernov/urlshortener/dto/user/UserLevelRequest.java
+++ src/main/java/ru/chernov/urlshortener/dto/user/UserLevelRequest.java
@@ -1,10 +1,14 @@
 package ru.chernov.urlshortener.dto.user;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.chernov.urlshortener.enums.user.UserLevelName;
 
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class UserLevelRequest {
     @NotNull
---
+++ src/test/resources/sql/user/update-level/success.sql
@@ -0,0 +1,2 @@
+INSERT INTO usr(id, username, password, status, user_level)
+VALUES (9000000101, 'test-username-for-getting', '12345', 'ACTIVE', 'NONE');
